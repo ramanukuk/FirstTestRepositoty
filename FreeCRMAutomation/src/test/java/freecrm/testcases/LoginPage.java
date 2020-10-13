a8b65f4cdc2178b72db371451e4c8923c72b3a9b
Home <Home@Home-Laptop.Home>
2020-10-13 12:50:23 +0100

    First Commit

---
+++ FreeCRMAutomation/src/test/java/freecrm/testcases/LoginPage.java
@@ -0,0 +1,5 @@
+package freecrm.testcases;
+
+public class LoginPage {
+
+}
---
+++ FreeCRMAutomation/src/test/java/freecrm/utility/BrowserFactory.java
@@ -0,0 +1,50 @@
+package freecrm.utility;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class BrowserFactory {
+
+	public static WebDriver startApplication(WebDriver driver, String browserName, String appURL) 
+	{
+	if(browserName.equals("Chrome"))
+	{
+		System.setProperty("webdriver.chrome.driver","./chromedriver.exe");
+		driver = new ChromeDriver();		
+
+		
+	}
+	else if (browserName.equals("Firefox"))
+	{
+		System.setProperty("webdriver.gecko.driver","./geckodriver.exe");
+		driver = new FirefoxDriver();		
+
+	}
+	else if (browserName.equals("IE"))
+	{
+		System.setProperty("webdriver.ie.driver","./IEDriverServer.exe");
+		driver = new InternetExplorerDriver();		
+
+	}
+	else
+	{
+		System.out.println("We do not support this browser");
+	}
+	
+	driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+	driver.manage().window().maximize();
+	driver.get(appURL);
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	
+	return driver;
+	
+	}
+	public static void quitBrowser(WebDriver driver)
+	{
+		driver.quit();
+	}
+	
+}
